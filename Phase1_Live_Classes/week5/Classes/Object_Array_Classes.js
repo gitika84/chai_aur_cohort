19f0773478f0f698e209c7ed62659d2677d83f61
gitika84 <gitikasharma8j@gmai.com>
2025-03-21 12:35:54 +0530

    another Question of array

--- Phase1_Live_Classes/week5/Classes/Object_Array_Classes.js
+++ Phase1_Live_Classes/week5/Classes/Object_Array_Classes.js
@@ -1,37 +1,47 @@
-
 const teas = [ "Green Tea" , "Black Tea" , "Oolong Tea" , "White Tea" , "Herbal Tea"];
-
 teas.push("Chemomile Tea")
 
-//
 
 const ind = teas.indexOf("Oolong Tea")
 if(ind > -1){
     teas.splice(ind , 1)
 }
 
-// 
 
 const caffeinated = teas.filter((caff) => caff != "Herbal Tea")
-// console.log(caffeinated)
+console.log(caffeinated)
 
-// console.log(teas.sort())
+console.log(teas.sort())
 
-// for(let i = 0; i < teas.length; i++){
-//     console.log(teas[i])
-// }
+for(let i = 0; i < teas.length; i++){
+    console.log(teas[i])
+}
 
-// let count = 0
-// for (let i = 0; i < teas.length; i++) {
-//     if(teas[i] != "Herbal Tea"){
-//         count++
-//     }
-// }
-// console.log(count)
+let count = 0
+for (let i = 0; i < teas.length; i++) {
+    if(teas[i] != "Herbal Tea"){
+        count++
+    }
+}
+console.log(count)
 
-// let newTeas = []
-// for(let i = 0 ; i < teas.length; i++){
-//     newTeas.push(teas[i].toUpperCase()) 
-// }
-// console.log(newTeas)
+let newTeas = []
+for(let i = 0 ; i < teas.length; i++){
+    newTeas.push(teas[i].toUpperCase()) 
+}
+console.log(newTeas)
 
+let mostChar = ""
+for(let i = 0; i < teas.length; i++){
+    if(teas[i].length > mostChar.length){
+        mostChar = teas[i]
+    }
+}
+console.log(mostChar)
+
+
+const reversedArray = []
+for(let i = teas.length - 1 ; i >= 0; i--){
+    reversedArray.push(teas[i])
+}
+console.log(reversedArray)
