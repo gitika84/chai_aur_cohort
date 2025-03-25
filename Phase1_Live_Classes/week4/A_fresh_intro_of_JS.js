3d0b152ec7c7ed4808ae17eee7fb32fcaa7f69d8
gitika84 <gitikasharma8j@gmai.com>
2025-03-25 23:39:18 +0530

    Another Classes completed

---
+++ Phase1_Live_Classes/week4/A_fresh_intro_of_JS.js
@@ -0,0 +1,24 @@
+
+// factory syntax
+// () parenthesis
+// {} currly brases
+
+function printChai(){
+    console.log("Hello Chai"); 
+}
+
+printChai()
+
+// `` back ticks
+
+function bringBrush(itne){
+    console.log(`Hnjii , le aaye ${itne} brush`);  
+}
+
+bringBrush(4);
+
+function addTwo (num1, num2){
+    return num1 + num2;
+}
+
+console.log(addTwo(2,3))
--- Phase1_Live_Classes/week5/Classes/Object_Array_Classes.js
+++ Phase1_Live_Classes/week5/Classes/Object_Array_Classes.js
@@ -1,47 +1,93 @@
-const teas = [ "Green Tea" , "Black Tea" , "Oolong Tea" , "White Tea" , "Herbal Tea"];
-teas.push("Chemomile Tea")
+// const teas = [ "Green Tea" , "Black Tea" , "Oolong Tea" , "White Tea" , "Herbal Tea"];
+// teas.push("Chemomile Tea")
 
 
-const ind = teas.indexOf("Oolong Tea")
-if(ind > -1){
-    teas.splice(ind , 1)
-}
+// const ind = teas.indexOf("Oolong Tea")
+// if(ind > -1){
+//     teas.splice(ind , 1)
+// }
 
 
-const caffeinated = teas.filter((caff) => caff != "Herbal Tea")
-console.log(caffeinated)
+// const caffeinated = teas.filter((caff) => caff != "Herbal Tea")
+// console.log(caffeinated)
 
-console.log(teas.sort())
+// console.log(teas.sort())
 
-for(let i = 0; i < teas.length; i++){
-    console.log(teas[i])
-}
+// for(let i = 0; i < teas.length; i++){
+//     console.log(teas[i])
+// }
 
-let count = 0
-for (let i = 0; i < teas.length; i++) {
-    if(teas[i] != "Herbal Tea"){
-        count++
+// let count = 0
+// for (let i = 0; i < teas.length; i++) {
+//     if(teas[i] != "Herbal Tea"){
+//         count++
+//     }
+// }
+// console.log(count)
+
+// let newTeas = []
+// for(let i = 0 ; i < teas.length; i++){
+//     newTeas.push(teas[i].toUpperCase()) 
+// }
+// console.log(newTeas)
+
+// let mostChar = ""
+// for(let i = 0; i < teas.length; i++){
+//     if(teas[i].length > mostChar.length){
+//         mostChar = teas[i]
+//     }
+// }
+// console.log(mostChar)
+
+
+// const reversedArray = []
+// for(let i = teas.length - 1 ; i >= 0; i--){
+//     reversedArray.push(teas[i])
+// }
+// console.log(reversedArray)
+
+// Objects
+
+const person = {
+    x : 10,
+    firstName : "Gitika",
+    lastName : "Sharma",
+    hobbies : ['Coding' , "Dance"],
+    isMarried : false,
+    getFullName : function(){
+        return "Geet"
+    },
+    address : {
+        hno : 1,
+        streat : 1,
+        countryCode : "IN",
+        state : "HR"
     }
 }
-console.log(count)
 
-let newTeas = []
-for(let i = 0 ; i < teas.length; i++){
-    newTeas.push(teas[i].toUpperCase()) 
+// console.log(person.hobbies)
+
+// const remote = {
+//     color: "Black",
+//     brand : "XYZ",
+//     dimensions : { height : 1 , width : 1},
+//     turnOff : function(){
+
+//     } ,
+//     volumeUp : function(){
+
+//     }
+// }
+
+
+let fname = "Geet"
+let fname2 = fname  // it copy the variable value
+fname2 = "Re"
+// console.log(fname2)
+
+let p1 = {
+    fname: "geet"
 }
-console.log(newTeas)
+let p2 = p1
+p2.fname = "MA"
 
-let mostChar = ""
-for(let i = 0; i < teas.length; i++){
-    if(teas[i].length > mostChar.length){
-        mostChar = teas[i]
-    }
-}
-console.log(mostChar)
-
-
-const reversedArray = []
-for(let i = teas.length - 1 ; i >= 0; i--){
-    reversedArray.push(teas[i])
-}
-console.log(reversedArray)
