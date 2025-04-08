5c66b097721ce6b373b7104c15f2a82fa53085cf
gitika84 <gitikasharma8j@gmai.com>
2025-04-08 17:13:40 +0530

    create a basic webserver structure

---
+++ Phase2_Live_Classes/Full_Stack/index.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import dotenv from "dotenv";
+import cors from 'cors';
+import db from './utils/db.js';
+import userRoutes from './routes/user.routes.js' 
+
+dotenv.config()
+
+const app = express()  // webserver
+
+app.use(cors({
+    origin: process.env.BASE_URL,
+    methods: [ "Get", "POST", "DELETE", "OPTIONS"] , // these are not case sensitive
+    allowedHeaders: ['Content-Type' , 'Authorization']  // case sensitive
+}))
+
+
+
+// for accept json data
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Cohort!')
+})   // this will work when request gone
+
+app.get('/peer', (req , res) => {
+    res.send("Peer!!")
+})
+
+// connect to db
+db()
+
+
+// user routes
+app.use('/api/v1/users', userRoutes)
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
