0c7a5fe75805e56bc9a5975a18e4e4351aa7d057
gitika84 <gitikasharma8j@gmai.com>
2025-05-14 12:55:24 +0530

    deleted a full_stack folder

--- Phase2_Live_Classes/Full_Stack/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { registerUser , verifyUser } from '../controller/user.controller.js'
-
-const router = express.Router()
-
-router.post('/register' , registerUser)
-router.get('/verify/:token', verifyUser )
-
-export default router
